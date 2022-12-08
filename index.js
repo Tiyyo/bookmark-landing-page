1fe12126bdc402d1f157c4c487826e55a126c7bf
Steeve Matou <steeve.matou@gmail.com>
2022-12-08 23:12:52 +0100

    email validation done

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+// console.log(isValidEmail); //true
+"use-strict";
+const formNewsletter = document.getElementById("signup-newsletter");
+const inputEmailNewsletter = document.getElementById("newsletter");
+const emailRegex = new RegExp(
+  /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/,
+  "gm"
+);
+const errorUi = document.getElementsByClassName("errors")[0];
 
+console.log(errorUi);
 
-const emailRegex = 
- new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+const emailValidation = () => {
+  let emailToTest = inputEmailNewsletter.value;
+  let errorEl = [errorUi, inputEmailNewsletter];
+  const isValidEmail = emailRegex.test(emailToTest);
+  console.log(isValidEmail);
+  if (isValidEmail === false) {
+    errorEl.forEach((el) => {
+      el.setAttribute("data-error-state", "active");
+    });
+  } else {
+    errorEl.forEach((el) => {
+      el.removeAttribute("data-error-state");
+    });
+    alert("You have signed up ;)");
+  }
+};
 
-const isValidEmail = emailRegex.test("email_address@domain.com");
-
-console.log(isValidEmail) //true
+formNewsletter.addEventListener("submit", (e) => {
+  e.preventDefault();
+  emailValidation();
+  inputEmailNewsletter.value = "";
+});
