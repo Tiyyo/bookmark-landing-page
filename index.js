c4e4affb26de5d8dba6159c0ae2bf09d650119a3
Steeve Matou <steeve.matou@gmail.com>
2022-12-10 02:03:48 +0100

    mobile version in progress

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const emailRegex = new RegExp(
   "gm"
 );
 const errorUi = document.getElementsByClassName("errors")[0];
+const arrows = document.querySelectorAll(".arrow");
+const accordionSection = document.querySelectorAll(".accordion--section");
+const tabLinks = document.querySelectorAll(".features__nav__link");
+const pageTab = document.querySelectorAll(".feature");
 
-console.log(errorUi);
+// console.log(pageTab);
 
 const emailValidation = () => {
   let emailToTest = inputEmailNewsletter.value;
@@ -27,8 +31,53 @@ const emailValidation = () => {
   }
 };
 
+const displayAnswerFaq = (arrow) => {
+  let questionId = arrow.dataset.arrowNumber;
+  for (let i = 0; i < accordionSection.length; i++) {
+    if (accordionSection[i].dataset.questionId === questionId) {
+      accordionSection[i].toggleAttribute("data-display-answer");
+      const isActive = accordionSection[i].hasAttribute("data-display-answer");
+      if (isActive === true) {
+        accordionSection[i].dataset.displayAnswer = "active";
+      }
+    }
+  }
+};
+
+const displayFeatures = (event) => {
+  let namePage = event.target.className.split(" ")[1];
+
+  for (let i = 0; i < tabLinks.length; i++) {
+    if (tabLinks[i].className.includes(namePage)) {
+      tabLinks[i].setAttribute("data-active", "true");
+    } else {
+      tabLinks[i].removeAttribute("data-active");
+    }
+  }
+
+  pageTab.forEach((page) => {
+    if (page.dataset.namePage === namePage) {
+      page.setAttribute("data-active", "true");
+    } else {
+      page.removeAttribute("data-active");
+    }
+  });
+};
+
 formNewsletter.addEventListener("submit", (e) => {
   e.preventDefault();
   emailValidation();
   inputEmailNewsletter.value = "";
+});
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    displayAnswerFaq(arrow);
+  });
+});
+
+tabLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    displayFeatures(e);
+  });
 });
